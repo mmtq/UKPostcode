56c997bac677bc903183573e6973b9050a914d8f
mmtq <mirtarhimul@gmail.com>
2024-09-19 01:00:40 +0600

    small changes

--- England/templatetags/custom_filters.py
+++ England/templatetags/custom_filters.py
@@ -1,7 +1,12 @@
 from django import template
+from django.utils.text import slugify
 
 register = template.Library()
 
 @register.filter(name='replace_spaces')
 def replace_spaces(value):
     return value.replace(' ', '-')
+
+@register.filter(name='slugify')
+def slugify_filter(value):
+    return slugify(value)
